b2ac16ab20a76ba372360ee667f3f121410f8aeb
priya171299 <priyakanwar2001@gmail.com>
2025-02-25 16:04:53 +0530

    inject new element

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,9 +1,10 @@
 import Youtube from "./Youtube";
 function App() {
+  const name = "Priya";
   return (
     <>
-      <h1>Vite react app</h1>
-      <h1>Vite react app</h1>
+      <h1>Vite react app {2 + 2}</h1>
+      <h1>Vite react app {name}</h1>
       <Youtube />
     </>
   );
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,6 +1,43 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React App</h1>
+    </div>
+  );
+}
+
+const AnotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit the Website
+  </a>
+);
+
+const areactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Click to Visit"
+);
+
+/*createRoot(document.getElementById("root")).render(areactElement);*/
+
+// createRoot(document.getElementById("root")).render(AnotherElement);
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+    <MyApp />
+  </>
+);
