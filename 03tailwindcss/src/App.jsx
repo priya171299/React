4723ce5948541ea0a2ecbc0de6d22f0e56558ff2
priya171299 <priyakanwar2001@gmail.com>
2025-03-05 15:20:03 +0530

    update page

--- 03tailwindcss/src/App.jsx
+++ 03tailwindcss/src/App.jsx
@@ -12,8 +12,16 @@ function App() {
       <h1 className="text-3xl bg-green-500 p-3 rounded-md">
         Vite with Tailwind
       </h1>
-      <Card />
-      <Card />
+      <Card
+        placeName="Egypt"
+        post="Egypt, country located in the northeastern corner of Africa."
+        image="https://s.yimg.com/fz/api/res/1.2/WdQGWqnQ9sbB7375taFY0g--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpbGw7aD0zODQ7cT04MDt3PTUxMg--/https://s.yimg.com/am/365d/654bcfe2deab08801b4007690574e3b6"
+      />
+      <Card
+        placeName="London"
+        post="Capital and largest city of England and the United Kingdom"
+        image="https://up.yimg.com/ib/th?asid=432345565109219518&id=OAUMA.A87BAABFBA96C715E135AC58BABDDA93_F4190A4784BAF4D3&pid=21.1&o=5&w=442&h=231&c=1&rs=1&qlt=95"
+      />
       <Card />
     </>
   );
