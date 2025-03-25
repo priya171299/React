ce52ec18098e7cd56de2fd643ef39579ac509669
priya171299 <priyakanwar2001@gmail.com>
2025-03-25 13:44:47 +0530

    create reactRoute app

---
+++ 07reactRouter/src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout.jsx";
+import About from "./component/About/About.jsx";
+import Home from "./component/Home/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
