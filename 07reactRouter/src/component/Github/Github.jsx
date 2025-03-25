1862a30fb1ac1b869c787e8567f176d25db79413
priya171299 <priyakanwar2001@gmail.com>
2025-03-25 16:58:50 +0530

    updated in routers

--- 07reactRouter/src/component/Github/Github.jsx
+++ 07reactRouter/src/component/Github/Github.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-//import { useLoaderData } from "react-router-dom";
+// import { useLoaderData } from "react-router-dom";
 
 function Github() {
-  //const data = useLoaderData();
+  //   const data = useLoaderData();
   const [data, setData] = useState({});
   useEffect(() => {
     try {
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from "react";
+import { Routes, Route, createRoutesFromElements } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -10,46 +11,46 @@ import Contact from "./component/Contact/Contact.jsx";
 import Users from "./component/Users/Users.jsx";
 import Github from "./component/Github/Github.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-      {
-        path: "user/:userId",
-        element: <Users />,
-      },
-      {
-        path: "github",
-        element: <Github />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />,
+//       },
+//       {
+//         path: "user/:userId",
+//         element: <Users />,
+//       },
+//       {
+//         path: "github",
+//         element: <Github />,
+//       },
+//     ],
+//   },
+// ]);
 
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path="/" element={<Layout />}>
-//       <Route path="" element={<Home />} />
-//       <Route path="about" element={<About />} />
-//       <Route path="contact" element={<Contact />} />
-//       <Route path="user/:userId" element={<Users />} />
-//       <Route loader={githubInfoLoader} path="github" element={<Github />} />
-//     </Route>
-//   )
-// );
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userId" element={<Users />} />
+      <Route path="github" element={<Github />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
