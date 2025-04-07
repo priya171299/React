5d55cc6d8c84a1bc05d4dfeddae03fcf48627b61
priya171299 <priyakanwar2001@gmail.com>
2025-04-07 15:25:10 +0530

    update in project

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -5,11 +5,44 @@ import "./App.css";
 import Header from "./component/Header/Header";
 import Home from "./component/Home/Home";
 import Footer from "./component/Footer/Footer";
+import Table from "react-bootstrap/Table";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
 }
 
 export default App;
